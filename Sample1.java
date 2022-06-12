c305f50bed162f89493ed322cdf62a0ea6597529
Himanshu-Roy-18 <royhimanshu18@gmail.com>
2022-06-12 10:20:23 +0530

    My First Commit

---
+++ Sample1.java
@@ -0,0 +1,6 @@
+class Sample{ 
+   public static void main(String[] args)  { 
+   System.out.println("This is java application by using docker ");
+   system.out.println("This is a Java File");
+   } 
+   }
